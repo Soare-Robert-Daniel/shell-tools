61cbe19218c76af9315fa9077ea25c03e236983d
Robert <soarerobert17@gmail.com>
2021-02-07 17:11:11 +0200

    Add Broot

--- tools.sh
+++ tools.sh
@@ -3,14 +3,17 @@
 if ! command -v micro &> /dev/null
 then
 	# install Micro
+	echo "Install Micro"
 	curl https://getmic.ro | bash
 	mv micro /usr/bin
 	export MICRO_TRUECOLOR=1
+	export TERM=xterm-256color
 fi
 
 if ! command -v starship &> /dev/null
 then
 	# install Starship theme
+	echo "Install Starship theme"
 	curl -fsSL https://starship.rs/install.sh | bash
 	echo 'eval "$(starship init bash)"' >> ~/.bashrc	
 fi
@@ -18,5 +21,16 @@ fi
 if ! command -v byobu &> /dev/null
 then
 	# Install a terminal multiplexer
+	echo "Install byobu"
 	sudo apt-get install byobu	
+fi
+
+if ! command -v br &> /dev/null
+then
+	# Install a file manager
+	echo "Install Broot"
+	wget https://dystroy.org/broot/download/x86_64-linux/broot
+	mv broot /usr/local/bin
+	sudo mv broot /usr/local/bin
+	sudo chmod +x /usr/local/bin/broot	
 fi
